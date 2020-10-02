4d01846df2f45e5a5ce9e38f14253e7496c7b5e3
Fly Higher <daniel_sy@live.com>
2020-10-02 11:14:27 +0800

    依赖升级

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,11 @@ tasks.withType<KotlinCompile> {
 }
 
 object Versions {
-    const val mirai = "1.2.2"
+    const val mirai = "1.3.1"
 
     object Kotlin {
-        const val compiler = "1.4.0"
-        const val stdlib = "1.4.0"
+        const val compiler = "1.4.10"
+        const val stdlib = "1.4.10"
         const val coroutines = "1.3.9"
         const val atomicFU = "0.14.4"
         const val serialization = "1.0.0-RC"
@@ -50,12 +50,12 @@ dependencies {
     implementation(ktor("auth-jwt"))
     implementation(ktor("serialization"))
 
-    implementation("me.liuwj.ktorm", "ktorm-core", "3.0.0")
-    implementation("me.liuwj.ktorm", "ktorm-support-postgresql", "3.0.0")
+    implementation("me.liuwj.ktorm", "ktorm-core", "3.1.0")
+    implementation("me.liuwj.ktorm", "ktorm-support-postgresql", "3.1.0")
     implementation("com.impossibl.pgjdbc-ng", "pgjdbc-ng", "0.8.4")
-    implementation("org.slf4j", "slf4j-simple", "1.7.29")
+    implementation("org.slf4j", "slf4j-simple", "1.7.30")
 
-    implementation("com.squareup.okhttp3", "okhttp", "4.7.2")
+    implementation("com.squareup.okhttp3", "okhttp", "4.9.0")
     implementation("org.jsoup", "jsoup", "1.13.1")
 
     implementation("com.google.zxing:core:3.4.0")
