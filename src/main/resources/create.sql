a01a594d6055260cb27545a148df56d7332d38f5
Fly Higher <daniel_sy@live.com>
2020-10-02 11:14:39 +0800

    SQL 格式化

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,39 +1,35 @@
-CREATE TABLE aki_rec_music(
+CREATE TABLE aki_rec_music (
     n serial PRIMARY KEY,
-
-    qq bigint NOT NULL,
-    sub_time timestamp NOT NULL,
-
-    like_num int NOT NULL default 0,
-    playlist_id int NOT NULL,
-    confirmed boolean NOT NULL default false,
-
-    title varchar(50) NOT NULL,
-    artist varchar(50) NOT NULL,
-    platform varchar(20) NOT NULL,
-
-    music_url varchar(250),
-    jump_url varchar(250) NOT NULL,
-    preview_url varchar(250) NOT NULL
+    qq BIGINT NOT NULL,
+    sub_time TIMESTAMP NOT NULL,
+    like_num INT NOT NULL DEFAULT 0,
+    playlist_id INT NOT NULL,
+    confirmed BOOLEAN NOT NULL DEFAULT FALSE,
+    title VARCHAR ( 50 ) NOT NULL,
+    artist VARCHAR ( 50 ) NOT NULL,
+    platform VARCHAR ( 20 ) NOT NULL,
+    music_url VARCHAR ( 250 ),
+    jump_url VARCHAR ( 250 ) NOT NULL,
+    preview_url VARCHAR ( 250 ) NOT NULL
 );
 
-CREATE TABLE aki_playlist(
+
+CREATE TABLE aki_playlist (
     n serial PRIMARY KEY,
-    start_time timestamp NOT NULL,
-    end_time timestamp
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP
 );
 
-CREATE TABLE aki_liked_music(
+CREATE TABLE aki_liked_music (
     n serial PRIMARY KEY,
-    qq bigint NOT NULL,
-    sub_time timestamp NOT NULL,
-
-    music_id int NOT NULL,
-    playlist_id int NOT NULL
+    qq BIGINT NOT NULL,
+    sub_time TIMESTAMP NOT NULL,
+    music_id INT NOT NULL,
+    playlist_id INT NOT NULL
 );
 
-CREATE TABLE aki_user(
-    qq bigint PRIMARY KEY,
-    level varchar(10) NOT NULL,
-    sub_chnotice boolean NOT NULL DEFAULT false
+CREATE TABLE aki_user (
+    qq BIGINT PRIMARY KEY,
+    level VARCHAR ( 10 ) NOT NULL,
+    sub_chnotice BOOLEAN NOT NULL DEFAULT FALSE
 );
